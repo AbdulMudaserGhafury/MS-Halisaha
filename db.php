36d25eeaeb1c892595f276740884f4fbf9c93a82
Abdul Mudaser Ghafury <148721753+AbdulMudaserGhafury@users.noreply.github.com>
2025-06-03 13:00:43 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$dbname = "deneme";
+$password = "Azizyar5151$";
+
+$conn = new mysqli($servername,$username ,$password, $dbname,);
+
+
+
+
+
+
+?>
